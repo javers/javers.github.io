7c266df3c442b92c018ce2bb10bd1b307e749fc0
Bartosz Walacik <bwalacik@gmail.com>
2015-12-02 19:04:32 +0100

    github API calling, sort

--- js/github-client.js
+++ js/github-client.js
@@ -8,11 +8,15 @@ var github = new Github({
 var repo = github.getRepo('javers', 'javers');
 
 repo.contributors(function (err, data) {
+    var sort_by_commit = function (a1, a2){
+        return a2.total - a1.total;
+    };
+
     if (err) {
         console.log(err)
     } else {
-        render_people(find_team(data), "#github-team");
-        render_people(find_contributors(data), "#github-contributors");
+        render_people(find_team(data).sort(sort_by_commit), "#github-team");
+        render_people(find_contributors(data).sort(sort_by_commit), "#github-contributors");
     }
 });
 
